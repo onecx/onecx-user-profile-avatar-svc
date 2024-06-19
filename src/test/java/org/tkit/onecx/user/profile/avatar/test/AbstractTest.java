01f4d5ec08a328d6c80ad13cfe2660ae9d74ed67
JordenReuter <149687553+JordenReuter@users.noreply.github.com>
2024-06-19 09:26:24 +0200

    feat: repo init and bare bone setup (#1)

---
+++ src/test/java/org/tkit/onecx/user/profile/avatar/test/AbstractTest.java
@@ -0,0 +1,65 @@
+package org.tkit.onecx.user.profile.avatar.test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import io.quarkus.test.keycloak.client.KeycloakTestClient;
+import io.restassured.config.RestAssuredConfig;
+import io.smallrye.jwt.build.Jwt;
+import io.smallrye.jwt.util.KeyUtils;
+import jakarta.json.Json;
+import jakarta.json.JsonObjectBuilder;
+import org.eclipse.microprofile.config.ConfigProvider;
+import org.eclipse.microprofile.jwt.Claims;
+
+import java.security.PrivateKey;
+
+import static com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS;
+import static io.restassured.RestAssured.config;
+import static io.restassured.config.ObjectMapperConfig.objectMapperConfig;
+
+@SuppressWarnings("java:S2187")
+public class AbstractTest {
+
+    protected static final String APM_HEADER_PARAM = "apm-principal-token";
+    protected static final String CLAIMS_ORG_ID = ConfigProvider.getConfig()
+            .getValue("%test.tkit.rs.context.tenant-id.mock.claim-org-id", String.class);;
+
+    static {
+        config = RestAssuredConfig.config().objectMapperConfig(
+                objectMapperConfig().jackson2ObjectMapperFactory(
+                        (cls, charset) -> {
+                            var objectMapper = new ObjectMapper();
+                            objectMapper.registerModule(new JavaTimeModule());
+                            objectMapper.configure(WRITE_DATES_AS_TIMESTAMPS, false);
+                            return objectMapper;
+                        }));
+    }
+
+    protected static final String USER_ALICE = "alice";
+
+    protected static final String USER_BOB = "bob";
+
+    KeycloakTestClient keycloakClient = new KeycloakTestClient();
+
+    protected String createAccessTokenBearer(String user) {
+        return "Bearer " + createAccessToken(user);
+    }
+
+    protected String createAccessToken(String user) {
+        return keycloakClient.getAccessToken(user);
+    }
+
+    protected static String createToken(String organizationId) {
+        try {
+            String userName = "test-user";
+            JsonObjectBuilder claims = Json.createObjectBuilder();
+            claims.add(Claims.preferred_username.name(), userName);
+            claims.add(Claims.sub.name(), userName);
+            claims.add(CLAIMS_ORG_ID, organizationId);
+            PrivateKey privateKey = KeyUtils.generateKeyPair(2048).getPrivate();
+            return Jwt.claims(claims.build()).sign(privateKey);
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+}
