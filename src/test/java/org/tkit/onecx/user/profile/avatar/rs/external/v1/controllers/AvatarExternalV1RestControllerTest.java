4b08db9c65d64c6c430dfa0adb585420f74786e2
Jorden_Reuter <jorden.reuter@capgemini.com>
2024-07-05 14:07:46 +0200

    feat: security integration and tests

--- src/test/java/org/tkit/onecx/user/profile/avatar/rs/external/v1/controllers/AvatarExternalV1RestControllerTest.java
+++ src/test/java/org/tkit/onecx/user/profile/avatar/rs/external/v1/controllers/AvatarExternalV1RestControllerTest.java
@@ -4,11 +4,13 @@ import static io.restassured.RestAssured.given;
 import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
 import static jakarta.ws.rs.core.Response.Status.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.tkit.quarkus.security.test.SecurityTestUtils.getKeycloakClientToken;
 
 import jakarta.ws.rs.core.HttpHeaders;
 
 import org.junit.jupiter.api.Test;
 import org.tkit.onecx.user.profile.avatar.test.AbstractTest;
+import org.tkit.quarkus.security.test.GenerateKeycloakClient;
 import org.tkit.quarkus.test.WithDBData;
 
 import gen.org.tkit.onecx.user.profile.avatar.rs.external.v1.model.RefTypeDTOV1;
@@ -18,6 +20,7 @@ import io.quarkus.test.junit.QuarkusTest;
 @QuarkusTest
 @TestHTTPEndpoint(AvatarExternalV1RestController.class)
 @WithDBData(value = "data/testdata.xml", deleteBeforeInsert = true, deleteAfterTest = true, rinseAndRepeat = true)
+@GenerateKeycloakClient(clientName = "testClient", scopes = { "ocx-up-avatar:read" })
 class AvatarExternalV1RestControllerTest extends AbstractTest {
 
     private static final String MEDIA_TYPE_IMAGE_JPG = "image/jpg";
@@ -27,6 +30,7 @@ class AvatarExternalV1RestControllerTest extends AbstractTest {
 
         var userId = "user1";
         var data = given()
+                .auth().oauth2(getKeycloakClientToken("testClient"))
                 .contentType(APPLICATION_JSON)
                 .pathParam("userId", userId)
                 .queryParam("refType", RefTypeDTOV1.MEDIUM)
@@ -43,6 +47,7 @@ class AvatarExternalV1RestControllerTest extends AbstractTest {
     void getMyImageJpgTest() {
 
         var data = given()
+                .auth().oauth2(getKeycloakClientToken("testClient"))
                 .contentType(APPLICATION_JSON)
                 .header(APM_HEADER_PARAM, createToken("user1", "tenant-100"))
                 .get("me")
@@ -54,6 +59,7 @@ class AvatarExternalV1RestControllerTest extends AbstractTest {
         assertThat(data).isNotNull().isNotEmpty();
 
         data = given()
+                .auth().oauth2(getKeycloakClientToken("testClient"))
                 .contentType(APPLICATION_JSON)
                 .queryParam("refType", RefTypeDTOV1.MEDIUM)
                 .header(APM_HEADER_PARAM, createToken("user1", "tenant-100"))
@@ -73,6 +79,7 @@ class AvatarExternalV1RestControllerTest extends AbstractTest {
         var refType = RefTypeDTOV1.MEDIUM;
 
         given()
+                .auth().oauth2(getKeycloakClientToken("testClient"))
                 .contentType(APPLICATION_JSON)
                 .pathParam("userId", userId + "_not_exists")
                 .queryParam("refType", refType)
---
+++ src/test/java/org/tkit/onecx/user/profile/avatar/test/SecurityTest.java
@@ -0,0 +1,18 @@
+package org.tkit.onecx.user.profile.avatar.test;
+
+import java.util.List;
+
+import org.tkit.quarkus.security.test.AbstractSecurityTest;
+import org.tkit.quarkus.security.test.SecurityTestConfig;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+public class SecurityTest extends AbstractSecurityTest {
+    @Override
+    public SecurityTestConfig getConfig() {
+        SecurityTestConfig config = new SecurityTestConfig();
+        config.addConfig("read", "/internal/avatar/id", 404, List.of("ocx-up:read"), "get");
+        return config;
+    }
+}
